640c5f3d8e94116f8ffe91c512394ea74867d6dc
ArturrrL <artik.levitskyi18@gmail.com>
2025-05-07 21:33:39 +0300

    added movie list

---
+++ absolutecinema/src/Data/Movies.js
@@ -0,0 +1,85 @@
+const movies = [
+    {
+      id: 1,
+      title: "Fast X: Part 2",
+      description: "Dom Toretto faces new high-speed challenges.",
+      genre: "Action",
+      poster: "https://m.media-amazon.com/images/M/MV5BMDQxZmM0MDYtYTM1Zi00YmRhLWJkMGEtNjk1MWY3MDUzZDlmXkEyXkFqcGc@._V1_.jpg",
+      trailer: "https://www.youtube.com/watch?v=Qs5-I7Ojeuw",
+      showtime: "2025-05-07 18:00"
+    },
+    {
+      id: 2,
+      title: "F1",
+      description: "A thrilling dive into Formula 1 racing.",
+      genre: "Sports Drama",
+      poster: "https://m.media-amazon.com/images/M/MV5BNGI0MDI4NjEtOWU3ZS00ODQyLWFhYTgtNGYxM2ZkM2Q2YjE3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
+      trailer: "https://www.youtube.com/watch?v=KsBNOAAXiCY",
+      showtime: "2025-05-07 19:00"
+    },
+    {
+      id: 3,
+      title: "Ford v Ferrari",
+      description: "The epic battle to build a revolutionary race car.",
+      genre: "Drama",
+      poster: "https://m.media-amazon.com/images/M/MV5BOTBjNTEyNjYtYjdkNi00YzE5LTljYzUtZjVlYmYwZmJmZWYxXkEyXkFqcGc@._V1_.jpg",
+      trailer: "https://www.youtube.com/watch?v=YVdUf8aEm1E",
+      showtime: "2025-05-07 20:00"
+    },
+    {
+      id: 4,
+      title: "Rush",
+      description: "The intense rivalry between F1 drivers in the 1970s.",
+      genre: "Biography",
+      poster: "https://m.media-amazon.com/images/M/MV5BMTZhOGQxM2ItNGQyYy00YzE5LWI5MjMtNmMzNGQzNDE1OTUzXkEyXkFqcGc@._V1_.jpg",
+      trailer: "https://www.youtube.com/watch?v=4XA73ni9eVs&pp=0gcJCdgAo7VqN5tD",
+      showtime: "2025-05-07 21:00"
+    },
+    {
+      id: 5,
+      title: "Gran Turismo",
+      description: "A gamer becomes a professional racer.",
+      genre: "Action",
+      poster: "https://m.media-amazon.com/images/M/MV5BMjA0N2YyNmYtZDk4Ny00ODE2LThmZWQtNGJiMDk0YzhiNzE5XkEyXkFqcGc@._V1_.jpg",
+      trailer: "https://www.youtube.com/watch?v=GgKmhDaVo48",
+      showtime: "2025-05-07 22:00"
+    },
+    {
+      id: 6,
+      title: "Champions League Final",
+      description: "Live screening of the UEFA Champions League Final.",
+      genre: "Sports",
+      poster: "https://tkalawatia.wordpress.com/wp-content/uploads/2015/06/29691ec800000578-3113812-image-a-40_14336266512631.jpg",
+      trailer: "https://www.youtube.com/watch?v=AHSawvm9nWI",
+      showtime: "2025-05-07 20:45"
+    },
+    {
+      id: 7,
+      title: "Minecraft",
+      description: "An adventure in a blocky, pixelated world.",
+      genre: "Adventure",
+      poster: "https://preview.redd.it/why-is-everyone-liking-the-minecraft-movie-after-the-second-v0-a4pu8ndutofe1.jpeg?width=640&crop=smart&auto=webp&s=dc6a8480329b79084a06f6ab7a7182693fa094e3",
+      trailer: "https://www.youtube.com/watch?v=41O_MydqxTU",
+      showtime: "2025-05-07 17:00"
+    },
+    {
+      id: 8,
+      title: "Spider-Man: No Way Home",
+      description: "Peter Parker faces multiversal threats.",
+      genre: "Superhero",
+      poster: "https://upload.wikimedia.org/wikipedia/ru/6/6e/Spider-Man_%E2%80%94_No_Way_Home_poster.jpg",
+      trailer: "https://www.youtube.com/watch?v=JfVOs4VSpmA",
+      showtime: "2025-05-07 18:30"
+    },
+    {
+      id: 9,
+      title: "Avengers",
+      description: "Earth's mightiest heroes unite to save the world.",
+      genre: "Superhero",
+      poster: "https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg",
+      trailer: "https://www.youtube.com/watch?v=TcMBFSGVi1c",
+      showtime: "2025-05-07 19:30"
+    }
+  ];
+  
+  export default movies;
