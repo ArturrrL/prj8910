ab92beec2381d3c772d456b3c512150097c4f9d0
ArturrrL <artik.levitskyi18@gmail.com>
2025-05-22 00:21:45 +0300

    App.jsx with  router-dom

--- absolutecinema/src/App.jsx
+++ absolutecinema/src/App.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import MovieList  from './components/MovieList';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Booking from './pages/Booking';
 import movies from './data/movies';
 
 function App() {
   return (
-    <div>
-      <header className="bg-black bg-opacity-80 py-6">
-        <h1 className="text-5xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
-          AbsoluteCinema
-        </h1>
-      </header>
-      <MovieList movies={movies} />
-    </div>
+    <Router>
+      <div>
+        <header className="bg-black bg-opacity-80 py-6">
+          <h1 className="text-5xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">
+            Cinema
+          </h1>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home movies={movies} />} />
+          <Route path="/booking/:id" element={<Booking movies={movies} />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
